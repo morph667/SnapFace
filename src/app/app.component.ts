116dc81f026239e2a202002e662cf7c69f13b7a2
barnab <babe89000@gmail.com>
2022-10-22 11:07:27 +0200

    Start development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'snapface';
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+
+  ngOnInit() {
+    this.mySnap = new FaceSnap(
+      'Archibald',
+      'Mon meilleur ami depuis tout petit !',
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      new Date(),
+      0
+    );
+    this.myOtherSnap = new FaceSnap(
+      'Alk',
+      'plus haut !',
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      new Date(),
+      12
+    );
+  }
 }
---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent implements OnInit {
+  @Input() faceSnap!: FaceSnap;
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+  buttonText!: string;
+
+  ngOnInit() {
+    this.title = 'Mirkovic';
+    this.description = 'Mon meilleur ami depuis tout petit !';
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.buttonText = 'Oh Snap!';
+
+  }
+
+  onSnap() {
+    if (this.buttonText === 'Oh Snap!') {
+      this.faceSnap.snaps++;
+      this.buttonText = 'Oops, unSnap!';
+    } else {
+      this.faceSnap.snaps--;
+      this.buttonText = 'Oh Snap!'
+    }
+  }
+}
+
